ea4fb7a3cea04ca2e34fc0c804d26991b3097ae8
ZV-96 <vanrooyenz@middleton.school.nz>
2022-04-07 19:08:54 +1200

    Update 01_yes_nov3.py

--- 01_yes_nov3.py
+++ 01_yes_nov3.py
@@ -1,16 +1,19 @@
-`# Ask the user if they have played before
-show_instructions = input("Have you played this game before?: ").lower()
+show_instructions = ""
+while show_instructions != "x":
 
-# if they say yes, output 'Program Continues'
-if show_instructions == "yes" or show_instructions == "y":
-    print("program continues")
+    # Ask the user if they have played before
+    show_instructions = input("Have you played this game before?: ").lower()
 
-# If they say no, output 'display Instructions'
-elif show_instructions == "no" or show_instructions == "n":
-    print("Display instructions")
+    # if they say yes, output 'Program Continues'
+    if show_instructions == "yes" or show_instructions == "y":
+        print("program continues")
 
-# Otherwise - show error
-else:
-    print("Please answer 'yes' or 'no'")
+    # If they say no, output 'display Instructions'
+    elif show_instructions == "no" or show_instructions == "n":
+        print("Display instructions")
 
-print(f"You entered '{show_instructions}'")
+    # Otherwise - show error
+    else:
+        print("Please answer 'yes' or 'no'")
+
+    print(f"You entered '{show_instructions}'")
