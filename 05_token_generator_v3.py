4ce448a69f1c1e8587fd4781028ad08fb08f6b39
ZV-96 <vanrooyenz@middleton.school.nz>
2022-04-12 14:41:23 +1200

    updates

---
+++ 05_token_generator_v3.py
@@ -0,0 +1,31 @@
+"""Component 3 (random tokens) v3
+Format currency
+Ensure odds favour the house - 10% chance of unicorn and 30% chance
+for each of the donkey, zebra, or horse"""
+
+
+import random
+
+tokens = ["unicorn",
+          "horse", "horse", "horse",
+          "donkey", "donkey", "donkey",
+          "zebra", "zebra", "zebra"]
+
+STARTING_BALANCE = 100
+balance = STARTING_BALANCE
+
+# testing loop to generate 20 tokens
+for item in range(100):
+    token = random.choice(tokens)
+
+    # adjust balance
+    if token == "unicorn":
+        balance += 4
+    elif token == "donkey":
+        balance -= 1
+    else:
+        balance -= .50
+
+# output
+print(f"Starting Balance = ${STARTING_BALANCE:.2f}")
+print(f"Final balance = ${balance:.2f}")
