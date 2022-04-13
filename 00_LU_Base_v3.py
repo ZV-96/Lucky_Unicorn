4ea57499fd1393eed2ef569bc1f588d5cc42c179
ZV-96 <vanrooyenz@middleton.school.nz>
2022-04-14 08:56:26 +1200

    Update 00_LU_Base_v3.py

--- 00_LU_Base_v3.py
+++ 00_LU_Base_v3.py
@@ -81,13 +81,14 @@ def generate_token(balance):
     # testing loop to generate 5 tokens
     while play_again != "x":
         rounds_played += 1  # keep track of rounds
+        print(formatter(".", f"Round {rounds_played}"))
+        print()
         number = random.randint(1, 100)  # can only be donkey
 
         # adjust balance
         # if the random number is between 1 and 5
         # user gets a unicorn and $4 to balance
         if 1 <= number <= 5:
-            token = "unicorn"
             balance += 4
             print(formatter("!", "Congratulations, you got a Unicorn"))
             print()
@@ -95,8 +96,8 @@ def generate_token(balance):
         # if the random number is between 6 and 36
         # user gets a donkey (subtract $1 from balance)
         elif 6 <= number <= 36:
-            token = "donkey"
             balance -= 1
+            print(formatter("0", "Bad Luck, you got a donkey"))
             print()
 
         # in all other cases the token must be a horse or a zebra
@@ -104,15 +105,17 @@ def generate_token(balance):
         else:
             # if number is even set the token to zebra
             if number % 2 == 0:
-                token = "zebra"
                 balance -= 0.5
+                print(formatter("Z", "You got a zebra"))
+                print()
             # otherwise, set the token to horse
             else:
-                token = "horse"
                 balance -= 0.5
+                print(formatter("H", "You got a horse"))
+                print()
 
         # output
-        print(f"Round {rounds_played}. Token: {token}, Balance: ${balance:.2f}")
+        print(f"Your balance is now: ${balance:.2f}")
         if balance < 1:
             print("\nSorry you have run out of money")
             play_again = "x"
@@ -143,6 +146,7 @@ if played_before == "No":
 # ask how much they want to play with
 starting_balance = num_check("How much do you want to play with $", 1, 10)
 print(f"you are playing with ${starting_balance}")
+print()
 
 closing_balance = generate_token(starting_balance)
 print("Thanks for playing!")
